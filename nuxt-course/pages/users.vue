4718a968d15c3af2f608ac79fac2e51006a25aa2
Hoang Bao Vu <baovuhbv@gmail.com>
2018-10-16 20:14:03 +0700

    Lession 15 - Creating Nested Routes

---
+++ nuxt-course/pages/users.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>The users view</h1>
+        <input type="text" v-model="userId">
+        <button @click="onLoadUser">Load User</button>
+        <nuxt-child />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            userId: ''
+        }
+    },
+    methods: {
+        onLoadUser() {
+            this.$router.push('/users/' + this.userId)
+        }
+    }
+}
+</script>
--- nuxt-course/pages/users/index.vue
+++ nuxt-course/pages/users/index.vue
@@ -1,22 +1,3 @@
 <template>
-    <div>
-        <h1>The users view</h1>
-        <input type="text" v-model="userId">
-        <button @click="onLoadUser">Load User</button>
-    </div>
+    <p>Please enter and id and click the button</p>
 </template>
-
-<script>
-export default {
-    data() {
-        return {
-            userId: ''
-        }
-    },
-    methods: {
-        onLoadUser() {
-            this.$router.push('/users/' + this.userId)
-        }
-    }
-}
-</script>
