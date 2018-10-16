7c52dab87f35c3f4778c20d4f0e67e15d8b2d79b
Hoang Bao Vu <baovuhbv@gmail.com>
2018-10-17 00:42:03 +0700

    Lesson 23 -Creating the Main Sections (Pages)

---
+++ nuxt-course/pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about-page">
+        <h1>About me</h1>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+    </div>
+</template>
+
+<style scoped>
+    .about-page {
+        text-align: center;
+        padding: 30px;
+    }
+</style>
+
