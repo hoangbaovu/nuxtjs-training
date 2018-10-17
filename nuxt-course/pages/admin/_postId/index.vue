c59912235c77e57b77ccbd6e4e50d391a36bc825
Hoang Bao Vu <baovuhbv@gmail.com>
2018-10-17 14:05:13 +0700

    Lesson_32._Preparing_the_Backend

---
+++ nuxt-course/pages/admin/_postId/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost" />
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+    components: {
+        AdminPostForm
+    },
+    data() {
+        return {
+            loadedPost: {
+                author: 'Neo',
+                title: 'My awesome Post',
+                content: 'Amazing post',
+                thumbnaiLink: 'https://images.viblo.asia/166bfddd-c9d9-4dc6-ac92-b5deb3d751c1.png'
+            }
+        }
+    }
+}
+</script>
+
--- nuxt-course/pages/admin/new-post/index.vue
+++ nuxt-course/pages/admin/new-post/index.vue
@@ -1,53 +1,30 @@
 <template>
   <section class="admin-new-post-page">
     <section class="admin-new-post-form">
-      <form @submit.prevent="onSave">
-        <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
-        <AppControlInput v-model="editedPost.title">Title</AppControlInput>
-        <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail</AppControlInput>
-        <AppControlInput
-          control-type="textarea"
-          v-model="editedPost.content">Content
-        </AppControlInput>
-        <AppButton type="submit">Save</AppButton>
-        <AppButton
-          btn-style="cancel"
-          style="margin-left: 10px"
-          type="button"
-          @click="onCancel">Cancel</AppButton>
-      </form>
+      <AdminPostForm />
     </section>
   </section>
 </template>
 
 <script>
-import AppButton from '@/components/UI/AppButton'
-import AppControlInput from '@/components/UI/AppControlInput'
+import AdminPostForm from '@/components/Admin/AdminPostForm'
 
 export default {
   components: {
-    AppButton,
-    AppControlInput
+    AdminPostForm
   },
-  data() {
-    return {
-      editedPost: {
-        author: '',
-        title: '',
-        thumbnailLink: '',
-        content: ''
-      }
-    }
-  },
-  methods: {
-    onSave() {
-      // Save the post
-      console.log(this.editedPost);
-    },
-    onCancel() {
-      // Navigation back
-      this.$router.push('/admin');
-    }
-  }
 }
 </script>
+
+<style scoped>
+.new-post-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .new-post-form {
+    width: 500px;
+  }
+}
+</style>
