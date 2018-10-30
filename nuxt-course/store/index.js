8e6c8eaaa5332fc5b2a2dadd997a273f8b7f50f4
Bao Vu Hoang <baovuhbv@gmail.com>
2018-10-30 23:59:46 +0700

    Lesson_54._Synchronizing_Vuex_and_the_back_end

--- nuxt-course/store/index.js
+++ nuxt-course/store/index.js
@@ -9,6 +9,13 @@ const createStore = () => {
     mutations: {
       setPosts(state, posts) {
         state.loadedPost = posts;
+      },
+      addPost(state, post) {
+        state.loadedPosts.push(post)
+      },
+      editPost(state, editedPost) {
+        const postIndex = state.loadedPosts.findIndex(post => post.id === editedPost.id );
+        state.loadedPosts[postIndex] = editedPost
       }
     },
     actions: {
@@ -22,6 +29,34 @@ const createStore = () => {
             vuexContext.commit('setPosts', postArray)
           })
           .catch(e => context.error(e));
+      },
+      addPost(vuexContext, post) {
+        const createdPost = {
+          ...post,
+          updatedDate: new Date()
+        }
+        return axios
+          .post('https://nuxt-blog-a4685.firebaseio.com/posts.json', createdPost)
+          .then(result => {
+            vuexContext.commit('addPost', { ...createdPost, id: result.data.name })
+            this.$route.push('admin')
+          })
+          .catch(e => console.log(e))
+      },
+      editPost(vuexContext, editedPost) {
+        return axios
+            .put(
+              'https://nuxt-blog-a4685.firebaseio.com/posts' +
+              editedPost.id +
+              '.json', editedPost
+            )
+            .then(res => {
+              vuexContext.commit('editPost', editedPost);
+            })
+            .catch(e => console.log(e));
+      },
+      setPosts(vuexContext, posts) {
+        vuexContext.commit("setPosts", posts);
       }
     },
     getters: {
