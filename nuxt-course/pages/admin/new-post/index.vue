4a9625e667f0fbb37fdb84dbe93978418828b3eb
Bao Vu Hoang <baovuhbv@gmail.com>
2018-10-30 21:09:33 +0700

    Lesson_52._Fetching_all_Post

--- nuxt-course/pages/admin/new-post/index.vue
+++ nuxt-course/pages/admin/new-post/index.vue
@@ -17,7 +17,11 @@ export default {
   },
   methods: {
     onSubmitted(postData) {
-      axios.post('https://nuxt-blog-a4685.firebaseio.com/posts.json', postData)
+      axios
+        .post('https://nuxt-blog-a4685.firebaseio.com/posts.json', {
+          ...postData,
+          updatedDate: new Date()
+        })
         .then(result => console.log(result))
         .catch(e => console.log(e))
     }
