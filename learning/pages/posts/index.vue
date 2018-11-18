6bebfeb3857f4702af47f140d70704f58b22a2f6
Bao Vu Hoang <baovuhbv@gmail.com>
2018-11-19 01:13:36 +0700

    Making API request - The Vue way

---
+++ learning/pages/posts/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Making API request - The Vue way</h2>
+
+    <h3 v-for="post in posts" v-bind:key="post.id">{{ post.title}}</h3>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+
+  mounted() {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+      .then(response => {
+        console.log(response);
+        this.posts = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+}
+</script>
