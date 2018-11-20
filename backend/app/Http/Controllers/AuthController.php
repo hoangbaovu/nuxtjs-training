70e3018d9304e9614ce48964687f61ae64b7797a
Bao Vu Hoang <baovuhbv@gmail.com>
2018-11-20 19:18:06 +0700

    Create validate request

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,16 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\User;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
+use App\Http\Requests\UserRegisterRequest;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
-        $this->validate($request, [
-            'email' => 'email|required|unique:users,email',
-            'name' => 'required',
-            'password' => 'required|min:6',
-        ]);
+    public function register(UserRegisterRequest $request) {
 
         $user = User::create([
             'email' => $request->email,
