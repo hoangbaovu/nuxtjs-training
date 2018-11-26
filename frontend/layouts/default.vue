b224fcae6533e4822ab9da1202d174a459747d23
Bao Vu Hoang <baovuhbv@gmail.com>
2018-11-26 16:11:45 +0700

    Frontend

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    <nuxt/>
+    <Navbar/>
+    <div class="container">
+      <nuxt/>
+    </div>
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar";
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
 
 <style>
 html {
---
+++ frontend/pages/register.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container mt-5 col-md-6">
+    <h2>Register</h2><br>
+    <form>
+      <div class="form-group">
+        <label>Full Name</label>
+        <input type="text" class="form-control" placeholder="Full Name">
+        <small class="form-text text-danger">Show errors here</small>
+      </div>
+      <div class="form-group">
+        <label>Email address</label>
+        <input type="email" class="form-control" placeholder="Enter email">
+        <small class="form-text text-danger">Show errors here</small>
+      </div>
+      <div class="form-group">
+        <label >Password</label>
+        <input type="password" class="form-control" placeholder="Password">
+        <small class="form-text text-danger">Show errors here</small>
+      </div>
+      <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+    <br>
+    <p>Don't have an account? <nuxt-link to="/register">Register</nuxt-link></p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
