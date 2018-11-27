6c3c959d4868a0da92368738dfefcf3d810535a3
Bao Vu Hoang <baovuhbv@gmail.com>
2018-11-27 23:31:52 +0700

    Scope trait

--- backend/app/Topic.php
+++ backend/app/Topic.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App;
+use App\Traits\Orderable;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model
 {
+    use Orderable;
+
     protected $fillable = ['title'];
 
     public function user() {
