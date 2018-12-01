61bb8ba121e6254b8f5d37306c618106d6288f01
Bao Vu Hoang <baovuhbv@gmail.com>
2018-12-01 11:00:26 +0700

    Nuxt - Get All Topics

---
+++ frontend/pages/topics/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h2>Latest Topics</h2>
+    <pre>
+      {{ topics }}
+    </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      topics: []
+    }
+  },
+  async asyncData({$axios}) {
+    let {data} = await $axios.$get('/topics');
+    // console.log(data);
+    return {
+      topics: data
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
