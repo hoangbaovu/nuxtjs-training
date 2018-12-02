05acd7df653784e573a471cd7198e9c40035b358
Bao Vu Hoang <baovuhbv@gmail.com>
2018-12-02 17:28:03 +0700

    Pagination

--- frontend/pages/topics/index.vue
+++ frontend/pages/topics/index.vue
@@ -9,9 +9,25 @@
         {{ content.body }}
       </div>
     </div>
-    <pre>
-      {{ topics }}
-    </pre>
+
+    <nav aria-label="...">
+      <ul class="pagination">
+        <li v-for="(key, value) in links" :key="value" class="page-item">
+          <a @click="loadMore(key)" href="#" class="page-link">{{value}}</a>
+        </li>
+        <!-- <li class="page-item disabled">
+          <a class="page-link" href="#" tabindex="-1">Previous</a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item active">
+          <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item">
+          <a class="page-link" href="#">Next</a>
+        </li> -->
+      </ul>
+    </nav>
   </div>
 </template>
 
@@ -19,14 +35,22 @@
 export default {
   data() {
     return {
-      topics: []
+      topics: [],
+      links: []
     }
   },
   async asyncData({$axios}) {
-    let {data} = await $axios.$get('/topics');
-    // console.log(data);
+    let {data, links} = await $axios.$get('/topics');
+    console.log(links);
     return {
-      topics: data
+      topics: data,
+      links
+    }
+  },
+  methods: {
+    async loadMore(key) {
+      let {data} = await this.$axios.$get(key);
+      return this.topics = {...this.topics, ...data};
     }
   }
 }
