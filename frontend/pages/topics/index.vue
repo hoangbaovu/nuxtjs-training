8350aa6e384702574baef085accaa7e756eb56a6
Bao Vu Hoang <baovuhbv@gmail.com>
2018-12-02 17:11:34 +0700

    Showing All Topics

--- frontend/pages/topics/index.vue
+++ frontend/pages/topics/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container">
     <h2>Latest Topics</h2>
+    <div v-for="(topic, index) in topics" :key="index" class="bg-light mt-5 mb-5">
+      <h3>{{ topic.title }}</h3>
+      <p class="text-muted">{{ topic.created_at }} by {{ topic.user.name }}</p>
+
+      <div v-for="(content, index) in topic.posts" :key="index" class="ml-5 content">
+        {{ content.body }}
+      </div>
+    </div>
     <pre>
       {{ topics }}
     </pre>
